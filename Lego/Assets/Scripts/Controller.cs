e9b01ef7911b3ceea3316aedb1399e73a09ac164
jamesshaw99 <james.shaw1234@hotmail.co.uk>
2020-08-17 18:00:43 +0100

    Check in 3
    Added character control and a jump animation

--- Lego/Assets/Scripts/Controller.cs
+++ Lego/Assets/Scripts/Controller.cs
@@ -1,55 +1,74 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+
+    //Input
     protected Player Player;
 
-    protected const float RotationSpeed = 10;
+    //Parameters
+    protected const float RotationSpeed = 100;
 
+    //Camera Controll
     public Vector3 CameraPivot;
     public float CameraDistance;
     protected float InputRotationX;
     protected float InputRotationY;
 
     protected Vector3 CharacterPivot;
-    protected Vector3 lookDirection;
+    protected Vector3 LookDirection;
+
     protected Vector3 LastMousePos;
 
-    private void Start()
+    // Use this for initialization
+    void Start()
     {
         Player = FindObjectOfType<Player>();
         LastMousePos = Input.mousePosition;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = false;
     }
 
-    private void FixedUpdate()
+    // Update is called once per frame
+    void FixedUpdate()
     {
-        var mouseDelta = Input.mousePosition - LastMousePos;
+        if (Input.GetKey(KeyCode.Q))
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
 
-        InputRotationX = InputRotationX + mouseDelta.x * RotationSpeed * Time.deltaTime % 360f;
-        InputRotationY = Mathf.Clamp(InputRotationY - mouseDelta.y * RotationSpeed * Time.deltaTime, -88f, 88f);
+        //scroll to zoom
+        CameraDistance = Mathf.Clamp(CameraDistance - Input.mouseScrollDelta.y, 0, 10);
 
+        //input
+        InputRotationX = InputRotationX + Input.GetAxis("Mouse X") * RotationSpeed * Time.deltaTime % 360f;
+        InputRotationY = Mathf.Clamp(InputRotationY - Input.GetAxis("Mouse Y") * RotationSpeed * Time.deltaTime, -88f, 88f);
+
+        //left and forward
         var characterForward = Quaternion.AngleAxis(InputRotationX, Vector3.up) * Vector3.forward;
         var characterLeft = Quaternion.AngleAxis(InputRotationX + 90, Vector3.up) * Vector3.forward;
 
-        var runDirection = characterForward * Input.GetAxisRaw("Vertical") + characterLeft * Input.GetAxisRaw("Horizontal"));
-        lookDirection = Quaternion.AngleAxis(InputRotationY, characterLeft) * characterForward;
+        //look and run direction
+        var runDirection = characterForward * Input.GetAxisRaw("Vertical") + characterLeft * Input.GetAxisRaw("Horizontal");
+        LookDirection = Quaternion.AngleAxis(InputRotationY, characterLeft) * characterForward;
 
+        //set player values
         Player.Input.RunX = runDirection.x;
         Player.Input.RunZ = runDirection.z;
-        Player.Input.LookX = lookDirection.x;
-        Player.Input.LookZ = lookDirection.z;
+        Player.Input.LookX = LookDirection.x;
+        Player.Input.LookZ = LookDirection.z;
         Player.Input.Jump = Input.GetKey(KeyCode.Space);
 
         CharacterPivot = Quaternion.AngleAxis(InputRotationX, Vector3.up) * CameraPivot;
 
-        LastMousePos = Input.mousePosition;
     }
 
     private void LateUpdate()
     {
-        Camera.main.transform.position = (transform.position + CharacterPivot) - lookDirection * CameraDistance;
-        Camera.main.transform.rotation = Quaternion.LookRotation(lookDirection, Vector3.up);
+        //set camera values
+        Camera.main.transform.position = (transform.position + CharacterPivot) - LookDirection * CameraDistance;
+        Camera.main.transform.rotation = Quaternion.LookRotation(LookDirection, Vector3.up);
     }
 }
---
+++ Lego/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    
+    protected Animator Animator;
+    protected Player Player;
+    protected Rigidbody Rigidbody;
+
+    void Awake()
+    {
+        Animator = GetComponent<Animator>();
+        Player = transform.parent.GetComponent<Player>();
+        Rigidbody = transform.parent.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Animator.SetFloat("Speed", Mathf.Sign(Vector3.Dot(Rigidbody.velocity, Rigidbody.transform.forward)) * Vector3.Project(Rigidbody.velocity, Rigidbody.transform.forward).magnitude);
+        Animator.SetFloat("Side", Mathf.Sign(Vector3.Dot(Rigidbody.velocity, Rigidbody.transform.right)) * Vector3.Project(Rigidbody.velocity, Rigidbody.transform.right).magnitude);
+        Animator.SetFloat("Jump", Mathf.Clamp(Player.DistanceFromGround(), 0, 1));
+    }
+}
